542d2cbb30337ae1b2916c15c5cdfe5e1b6e5f84
J K <surhommekim@gmail.com>
2019-09-25 11:36:06 +0900

    mina deploy test

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
+set :application_name, 'MinaDeployTestMyApp'
+set :domain, '13.209.64.175'
+set :deploy_to, '/home/ec2_user/MinaDeployTestMyApp'
+set :repository, 'https://github.com/KIMJUNGKWON/mina_deploy_test.git'
+set :branch, 'master'
+
+set :identity_file, "#{ENV["HOME"]}/Desktop/Lab/Utils/pem/surhommekim.pem"
+set :execution_mode, :system
+
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :remote_environment do
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+end
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0 --skip-existing}
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    # invoke :'rails:db_migrate'
+    # invoke :'rails:assets_precompile'
+    # invoke :'deploy:cleanup'
+
+    # on :launch do
+    #   in_path(fetch(:current_path)) do
+    #     command %{mkdir -p tmp/}
+    #     command %{touch tmp/restart.txt}
+    #   end
+    # end
+  end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - https://github.com/mina-deploy/mina/tree/master/docs
